5a87948b4299f004f4eed4496e6f8b6da15c83f3
Noopur Khatri <noopurkhatri41434@gmail.com>
2025-04-14 23:53:52 +0530

    Login function encrypted password check

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,8 +27,32 @@ export const register = async (req,res)=>{
         res.status(500).json({message: "Failed to create user!"});
     }
 };
-export const login = (req,res)=>{
+export const login = async (req,res)=>{
+    const {username,password} = req.body;
 
+    try{
+        //CHECK IF THE USER EXISTS
+
+        const user = await prisma.user.findUnique({
+            where:{username}
+        })
+
+        if(!user) return res.status(401).json({message:"Invalid Credentials!"});
+
+        //CHECK IF THE PASSWORD IS CORRECT
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordValid) return res.status(401).json({message: "Invalid Credentials!"});
+
+        //GENERATE COOKIE TOKEN AND SEND TO THE USER IF PASSWORD CORRECT
+
+        res.setHeader("Set-Cookie","test="+"myValue").json("success");
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:"Failed to login!"});
+    }
 }
 export const logout = (req,res)=>{
     //db operations
