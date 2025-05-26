ae87423297d846682abbc660e8319d9ab37e3e5e
Noopur Khatri <noopurkhatri41434@gmail.com>
2025-05-27 03:55:56 +0530

    register and login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -59,11 +59,13 @@ export const login = async (req,res)=>{
         {expiresIn: age} //after one week we wont be able to use this token as it will expire
     );
 
+    const {password: userPassword, ...userInfo} = user
+
         res.cookie("token",token,{
             httpOnly:true,
             //secure:true, ->cant do as of now as we're using localhost but make true in production mode
             maxAge: age,
-        }).status(200).json({message: "Login Successful!"});
+        }).status(200).json(userInfo);
 
     }catch(err){
         console.log(err);
