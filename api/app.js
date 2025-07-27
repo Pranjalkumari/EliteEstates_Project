ef42eff191827f7f7b076e4e9d2c743b0ba117cb
Pranjalkumari <kumaripranjal694@gmail.com>
2025-07-28 01:55:57 +0530

    creating chat option

--- api/app.js
+++ api/app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv'
 import cookieParser from "cookie-parser";
-import authRoute from "./routes/auth.route.js";
-import postRoute from "./routes/post.route.js";
-import testRoute from "./routes/test.route.js";
-import userRoute from "./routes/user.route.js";
 
 
 
+
+dotenv.config();
 
 const app = express();
 
@@ -15,10 +14,20 @@ app.use(cors({origin:process.env.CLIENT_URL, credentials: true}))
 app.use(express.json());
 app.use(cookieParser());
 
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+
 app.use("/api/auth", authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/posts", postRoute); 
 app.use("/api/test",testRoute);
+app.use("/api/chats",chatRoute);
+app.use("/api/messages",messageRoute);
+
 
 
 
---
+++ api/routes/message.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { 
+ addMessage
+    } 
+    from "../controllers/message.controller.js";
+import {verifyToken} from "../middleware/verifyToken.js"
+
+const router = express.Router();
+
+router.get("/", verifyToken,addMessage);
+
+
+export default router; 
